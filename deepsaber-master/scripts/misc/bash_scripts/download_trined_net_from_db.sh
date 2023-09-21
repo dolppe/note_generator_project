8b89ed55f03fce55ba71b484da537c9e09f987dd
dolppe <dbtjdals9342@naver.com>
2023-09-21 14:43:41 +0900

    파일 구조 변경

--- deepsaber-master/scripts/misc/bash_scripts/download_trined_net_from_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../dropbox_uploader.sh download /deepsaber_trained_nets/${1}/iter_${2}_net_.pth base/${1}/iter_${2}_net_.pth 
--- note_generator/scripts/misc/bash_scripts/upload_trained_net_to_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../dropbox_uploader.sh upload base/${1}/iter_${2}_net_.pth /deepsaber_trained_nets/${1}/iter_${2}_net_.pth
